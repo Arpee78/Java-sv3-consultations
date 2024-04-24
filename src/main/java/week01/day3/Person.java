850cad3efdb7035d0fef77ddd3f2a5443d75d72b
Arpee78 <arpee78@gmail.com>
2024-04-24 21:05:02 +0200

    ex-w01d03

---
+++ src/main/java/week01/day3/Person.java
@@ -0,0 +1,10 @@
+package week01.day3;
+
+public class Person {
+
+    public static void main(String[] args) {
+        System.out.println("Név: Teszt Elek");
+        System.out.println("Születési hely, idő: Budapest, 1983.05.27.");
+        System.out.println("Anyja neve: Minta Gizella");
+    }
+}
