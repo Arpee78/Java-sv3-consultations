306997b3a6eb35cecde0e4e3f7256138181ebfb0
Arpee78 <arpee78@gmail.com>
2024-05-24 19:57:59 +0200

    ex-w01d05

---
+++ src/main/java/week01/day05/Geometry.java
@@ -0,0 +1,12 @@
+package week01.day05;
+
+public class Geometry {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(5, 6);
+        System.out.println("Az A oldal hossza: " + rectangle.getSideA());
+        System.out.println("A B oldal hossza: " + rectangle.getSideB());
+        System.out.println("A téglalap területe: " + rectangle.calculateArea());
+    }
+}
